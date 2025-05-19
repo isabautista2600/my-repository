cdfcb5a456c950887b1f7a73647ba0d3d9b44740
isabautista2600 <97646920+isabautista2600@users.noreply.github.com>
2025-05-18 21:21:09 -0600

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -19,6 +19,8 @@ var speakWord = "Good Bye";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
+  window.byeSpeaker = byeoSpeaker
+})(window);
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
